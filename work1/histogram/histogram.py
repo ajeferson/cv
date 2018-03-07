60ec243535c3d67026f778af26828fd2bb3c6b2b
Alan Jeferson <alan.jeferson11@gmail.com>
2018-03-07 18:19:02 -0300

    Add histogram

---
+++ work1/histogram/histogram.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def histogram(filename):
+    result = np.zeros(256)
+    image = cv2.imread(filename, 0)
+    height, width = image.shape
+    for i in range(height):
+        for j in range(width):
+            result[image[i, j]] += 1
+
+    w = 0.5
+    plt.bar(range(256), result)
+    plt.show()
+
+
+histogram('lena.png')
+
